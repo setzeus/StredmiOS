63a5df70a22179b7ea4bd53727f2d7fe7d99cd62
theprinc <jnajera1917@gmail.com>
2014-03-09 18:26:26 -0400

    Init basic menu

---
+++ StredmiOS/JNMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  JNMenuViewController.h
+//  StredmiOS
+//
+//  Created by Jesus Najera on 3/9/14.
+//  Copyright (c) 2014 Stredm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JNMenuViewController : UITableViewController
+- (IBAction)unwindToMenuViewController:(UIStoryboardSegue *)segue;
+@end
---
+++ StredmiOS/JNMenuViewController.m
@@ -0,0 +1,17 @@
+//
+//  JNMenuViewController.m
+//  StredmiOS
+//
+//  Created by Jesus Najera on 3/9/14.
+//  Copyright (c) 2014 Stredm. All rights reserved.
+//
+
+#import "JNMenuViewController.h"
+
+@interface JNMenuViewController ()
+
+@end
+
+@implementation JNMenuViewController
+- (IBAction)unwindToMenuViewController:(UIStoryboardSegue *)segue; { }
+@end
