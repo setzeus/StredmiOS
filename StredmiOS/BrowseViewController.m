bf2e29114b610fa34a0587ded6de09784242ae33
theprinc <jnajera1917@gmail.com>
2014-03-15 20:14:34 -0400

    changes

--- StredmiOS/BrowseViewController.m
+++ StredmiOS/BrowseViewController.m
@@ -104,7 +104,8 @@
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    [[UISegmentedControl appearance] backgroundImageForState: barMetrics:<#(UIBarMetrics)#>
+   // UIImage *segmentedBackground = [UIImage imageNamed:@"GearImage.png"];
+
 }
 
 - (void)didReceiveMemoryWarning
